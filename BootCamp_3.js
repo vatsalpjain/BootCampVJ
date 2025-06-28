9c960733471e95203f0286e66208d1c1b310a568
Vatsal Jain <vatsalpjain@gmail.com>
2025-06-28 13:23:27 +0530

    all together

---
+++ BootCamp_3.js
@@ -0,0 +1,14 @@
+console.log('Hello');
+function sayHello() {
+    console.log('Hello, World!');
+}
+let people = 5;
+function sayBye(people) {
+    console.log('bye population: ' + people);
+}
+function sayHelloTo(name) {
+    console.log('Hello, ' + name + '!');
+}
+
+sayHello();
+sayHello();
